e559e16cd28b1f83215e17c7482378a7292588c0
Emmanuel <52438592+EmmanuelAudu@users.noreply.github.com>
2023-02-14 21:56:19 -0800

    Update App.jsx

--- micro-service-starter/docker_code/ui-web-app-reactjs/src/components/App.jsx
+++ micro-service-starter/docker_code/ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://34.125.225.159:9999/'+e.target.name;
+    const url = 'http://35.88.140.238:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
